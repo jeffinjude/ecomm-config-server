d266509c81eda1c6b3806d8b9ff074524f48cc53
jeffinjude <jeffinjude@gmail.com>
2024-04-30 18:52:59 +0530

    initial code structure

---
+++ src/test/java/com/jeffinjude/configserver/EcommConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeffinjude.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
